076c97088242725e30976744cc961c925c5578d9
Jana Stinn <jana-stinn@gmx.de>
2019-01-27 19:52:16 +0100

    changed home html

--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -1,41 +1,15 @@
 "use strict";
 
-let getPostsList = async () => {
-  const options = {
-     method: 'GET',
-     headers: {
-         'Content-Type': 'application/json'
-     }
- };
- try {
-     const response = await fetch(`https://5bb634f6695f8d001496c082.mockapi.io/api/posts`, options)
-     const json = await response.json();
-     // console.log(json)
-     return json
- } catch (err) {
-     console.log('Error getting documents', err)
- }
-}
-
 let Home = {
- render : async () => {
-     let posts = await getPostsList()
-     let view = `
-         <section class="section">
-             <h1> Home </h1>
-             <ul>
-                 ${ posts.map(post => 
-                     /*html*/`<li><a href="#/p/${post.id}">${post.title}</a></li>`
-                     ).join('\n ')
-                 }
-             </ul>
-         </section>
-     `
-     return view
- }
- , after_render: async () => {
- }
-
-}
+    render : async () => {
+        let view = `
+        <section class="section">
+            <h1> Home </h1>
+        </section>
+        `
+        return view
+    },
+    after_render: async () => {}
+    }
 
 export default Home;
