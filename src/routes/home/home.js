2bf4c172558935d680a8e074dcd2bd0956db178c
Jana Stinn <jana-stinn@gmx.de>
2019-02-10 16:35:08 +0100

    first try google maps implementation

--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -6,32 +6,32 @@ import imageMain from '../../assets/main_alternative.jpg';
 import imageSecond from '../../assets/mandeln.jpg';
 
 let Home = {
-    render : async () => {
-        let view = `
-          <section class="home">
-            <div class="home-r1">
-              <img src="${imageMain}" class="home-r1_img" alt=""/>
+  render : async () => {
+      let view = `
+        <section class="home">
+          <div class="home-r1">
+            <img src="${imageMain}" class="home-r1_img" alt=""/>
+          </div>
+          <h1 class="home-headline">
+            <div><span class="home-plus">+</span>Aveda</div>
+            <div>&nbsp;&nbsp;&nbsp;Mission</div>
+          </h1>
+          <div class="home-r2">
+            <div class="home-r2c1">
+              <img src="${imageSecond}" class="home-r2_img" alt=""/>
             </div>
-            <h1 class="home-headline">
-              <div><span class="home-plus">+</span>Aveda</div>
-              <div>&nbsp;&nbsp;&nbsp;Mission</div>
-            </h1>
-            <div class="home-r2">
-              <div class="home-r2c1">
-                <img src="${imageSecond}" class="home-r2_img" alt=""/>
+            <div class="home-r2c2">
+              <p class="home-plus">+&nbsp;&nbsp;&nbsp;+&nbsp;&nbsp;&nbsp;+</p>
+              <h2 class="home-r2c2_headline">"LIEBE IST DER URSPRUNG ALLER DINGE"</h2>
+              <p class="home-r2c2_text">5,000 Jahre Erfahrung liegen in der traditionellen indischen Heillehre des Ayurveda, die auf dem Wissen des Lebens und der Verbundenheit aller Dinge beruht.</p>
+              <p class="home-r2c2_text">Die Ziele und Herzensangelegenheiten von Horst gewannen eigenen Impetus im Zusammenhang mit einer größeren globalen Bewegung hin zu Ausgeglichenheit und Harmonie für den Einzelnen und die Gemeinschaft.</p>
               </div>
-              <div class="home-r2c2">
-                <p class="home-plus">+&nbsp;&nbsp;&nbsp;+&nbsp;&nbsp;&nbsp;+</p>
-                <h2 class="home-r2c2_headline">"LIEBE IST DER URSPRUNG ALLER DINGE"</h2>
-                <p class="home-r2c2_text">5,000 Jahre Erfahrung liegen in der traditionellen indischen Heillehre des Ayurveda, die auf dem Wissen des Lebens und der Verbundenheit aller Dinge beruht.</p>
-                <p class="home-r2c2_text">Die Ziele und Herzensangelegenheiten von Horst gewannen eigenen Impetus im Zusammenhang mit einer größeren globalen Bewegung hin zu Ausgeglichenheit und Harmonie für den Einzelnen und die Gemeinschaft.</p>
-                </div>
-            </div>
-          </section>
-        `
-        return view
-    },
-    after_render: async () => {}
-    }
+          </div>
+        </section>
+      `
+      return view
+  },
+  after_render: async () => {}
+}
 
 export default Home;
--- src/routes/kontakt/kontakt.js
+++ src/routes/kontakt/kontakt.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import './kontakt.scss';
+
 let getOpeningHours = async () => {
   try {
     const response = await fetch(`../../data.json`)
@@ -10,18 +12,25 @@ let getOpeningHours = async () => {
   } 
 }
 
+window.initMap = () => {
+  var heckmann = {lat: 50.9974041, lng: 6.8038675};
+  var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: heckmann});
+  var marker = new google.maps.Marker({position: heckmann, map: map});
+}
+
 let Kontakt = {
   render : async () => {
-    let openingHours = await getOpeningHours()
+    let openingHours = await getOpeningHours();
     let view = `
-      <ul>
-          ${ openingHours.map(openingHour => 
-              `<li>${openingHour.day} ${openingHour.time}</li>`
-              )
-          }
-      </ul>
+    <ul>
+    ${ openingHours.map(openingHour => 
+      `<li>${openingHour.day} ${openingHour.time}</li>`
+      )
+    }
+    </ul>
+    <div id="map"></div>
     `
-    return view
+    return view;
   },
   after_render: async () => {}
 }
