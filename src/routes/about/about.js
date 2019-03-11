fb61658da739ee01bd4a6f2c8ea1773c5d7f4b2b
lindaluisa <lindaluisarosenthal@gmail.com>
2019-03-11 23:34:07 +0100

    tablet responsive

--- src/routes/about/about.js
+++ src/routes/about/about.js
@@ -11,13 +11,14 @@ let About = {
         <section class="about">
           <img class="about-image about-image__1" src="${imageAbout1}" class="home-r3_img" alt=""/>
           <div class="about-container"</div>
-          <div><span class="about-plus home-plus-aveda">+  </span></div>
+          <div><span class="about-plus">+  </span></div>
               <h1 class="about-headline"> About </h1>
           </div>
           <div class="about-image__2-wrapper">
           <div class="about-text">
           <p>Vor nun genau 27 Jahren eröffnete Petra Heckmann in Pulheim. Seit dem Beginn hat sich viel getan. Der Betrieb ist gewachsen und befindet sich seit 2008 in neuen, modernen und lichtdurchfluteten Räumlichkeiten. Das Team umfasst mittlerweile 7 Mitarbeiter aller Altersklassen, welche sich durch stetige Weiterbildung, Sorgfalt, Kreativität aber vor allem Liebe zum Beruf auszeichnen.</p>
           <p>Egal ob klassische Friseurdienstleitsungen, Festfrisuren, Make-Up und Kosmetik, Haarverlängerungen von Great Lengths, Calligraphy Cut oder Pflege und Wellness von Aveda.</p>
+          <div class="about-plus__plus">+ + +</div>
           </div>
           </div>
           <img class="about-image about-image__2" src="${imageAbout2}" class="home-r3_img" alt=""/>
